71e4c323427072ce2748c4d3018b8d7e1dbc06a9
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2018-12-01 22:44:35 +0100

    Update custom_funct.js

--- leaflet/JS-CSS/custom_funct.js
+++ leaflet/JS-CSS/custom_funct.js
@@ -6,6 +6,6 @@ function leaflet_alert2() {
   var txt;
   var r = confirm("By clicking OK the user takes full responsibility of using this map and absolves the map developper of any responsibility.");
   if (r != true) {
-      window.open('https://edrap.github.io/abruzzomap', '_blank');
+      window.open('https://edrap.github.io/abruzzomap');
   }
 }
