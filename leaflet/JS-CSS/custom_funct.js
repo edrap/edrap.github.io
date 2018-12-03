f8850307af6b4032c5e636f66346b2a52de26451
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2018-12-03 14:07:24 +0100

    Update custom_funct.js

--- leaflet/JS-CSS/custom_funct.js
+++ leaflet/JS-CSS/custom_funct.js
@@ -1,5 +1,5 @@
 function leaflet_alert() {
-    alert("By clicking OK the user takes full responsibility of using this map and absolves the map developper of any responsibility.");
+    alert("By using this map the user takes full responsibility of the risks involved and absolves the map developper of any responsibility.");
 }
 
 function leaflet_confirm() {
