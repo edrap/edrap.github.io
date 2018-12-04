c4768ad5f81659614c7dba56de52fec7f56098c3
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2018-12-04 17:38:41 +0100

    Update custom_funct.js

--- leaflet/JS-CSS/custom_funct.js
+++ leaflet/JS-CSS/custom_funct.js
@@ -1,3 +1,3 @@
 function leaflet_alert() {
-    alert("The users take full responsibility of using the map for outdoor activities");
+    alert("The users take full responsibility for using the map for outdoor activities");
 }
