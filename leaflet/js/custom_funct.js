387a0ffbd4986dabeaa03d4274f229462843a52f
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2020-04-04 12:47:16 +0200

    Update custom_funct.js

--- leaflet/js/custom_funct.js
+++ leaflet/js/custom_funct.js
@@ -1,3 +1,12 @@
 function leaflet_alert() {
     alert("The users take full responsibility for using the map for outdoor activities");
 }
+
+function onEachFeature(feature, layer) {
+  //var popupContent = null;
+  if (feature.properties && feature.properties.description) {
+    //popupContent += feature.properties.description;
+    popupContent = feature.properties.description;
+  }
+  layer.bindPopup(popupContent, {maxWidth: "200", maxHeight : "300", closeOnClick: true, closeButton: false});
+}
