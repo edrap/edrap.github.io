0b4e45288ce7f0e1ffac6c10ce38a0e796902a12
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2020-04-23 11:33:37 +0200

    Update custom_funct.js

--- leaflet/js/custom_funct.js
+++ leaflet/js/custom_funct.js
@@ -17,3 +17,20 @@ function onEachFeature(feature, layer) {
   });  
   //layer.bringToFront();
 }
+
+function onEachLoadedFeature(feature, layer) {
+  //var popupContent = null;
+  //console.log(feature)
+  if (feature.properties && feature.properties.name) {
+    //popupContent += feature.properties.description;
+    popupContent = feature.properties.name;
+  }
+  layer.bindPopup(popupContent, {
+    maxWidth: "200",
+    maxHeight : "300",
+    closeOnClick: true,
+    closeButton: false,
+    //offset: [10, 10]
+  });
+  //layer.bringToFront();
+}
