f6c53315c1f45af9d56d4a18e6c424082d6da2f4
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2021-02-14 22:14:41 +0100

    Update abruzzomap.js

--- leaflet/js/abruzzomap.js
+++ leaflet/js/abruzzomap.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
   var tcdlayer = L.esri.imageMapLayer({
     //url: "https://image.discomap.eea.europa.eu/arcgis/rest/services/GioLandPublic/HRL_TreeCoverDensity_2018/ImageServer",
     url: "https://image.discomap.eea.europa.eu/arcgis/rest/services/GioLandPublic/HRL_DominantLeafType_2018/ImageServer",
-    opacity: 0.3,
+    opacity: 0.25,
     maxZoom: 16,
     maxNativeZoom: 16,
     minNativeZoom: 10,
