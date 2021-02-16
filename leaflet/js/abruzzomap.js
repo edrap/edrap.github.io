0e5224d9cf83ec013c2d909760b52e5a9e3c1e32
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2021-02-16 14:23:40 +0100

    Update abruzzomap.js

--- leaflet/js/abruzzomap.js
+++ leaflet/js/abruzzomap.js
@@ -319,7 +319,7 @@ $(document).ready(function () {
     tc_dlt_colors = ["#469e4a", "#1c5c24"]
     labels = ['<strong> Leaf type </strong>'];
       // loop through our density intervals and generate a label with a colored square for each interval
-    div.innerHTML += '<br>' + '<br>' + labels + '<br>';
+    div.innerHTML += '<br>' + labels + '<br>';
       for (var i = 0; i < tc_dlt.length; i++) {
         div.innerHTML +=
         '<i style="background:' + tc_dlt_colors[i] + '"></i> ' +
