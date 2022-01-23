68278ed06289dfef80376e4a4dfab8bc590a9198
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2022-01-23 21:17:09 +0100

    Update abruzzomap_clpv.js

--- leaflet/js/abruzzomap_clpv.js
+++ leaflet/js/abruzzomap_clpv.js
@@ -327,7 +327,7 @@ $(document).ready(function () {
   // ----------- LAYERS ----------
   // var baseMaps = {'Mapy Winter':mapy_winter, '4UMaps':baselayer2, 'OpenTopoMap':baselayer, 'Igm25k Min':igm25k_min, 'Igm25k Reg':igm25k_reg, 'Google Hybrid':googleHybrid};
   var baseMaps = {'Mapy Winter':mapy_winter, '4UMaps':baselayer2, 'OpenTopoMap':baselayer, 'Google Hybrid':googleHybrid, 'IGM Italia':igm25k_min, 'IGM Abruzzo':igm25k_reg};
-  var overlayMaps = {'Tree cover (2015)':tcdlayer, 'Hillshade':hillshlayer, 'Slope class':slopelayer, 'C.L.P.V VA':avalanches_va, 'C.L.P.V VF':avalanches_vf, 'Ski Piste':pistelayer, 'User features':userFeatures};
+  var overlayMaps = {'Tree cover (2015)':tcdlayer, 'Hillshade':hillshlayer, 'Slope class':slopelayer, 'C.L.P.V. VA':avalanches_va, 'C.L.P.V. VF':avalanches_vf, 'Ski Piste':pistelayer, 'User features':userFeatures};
   L.control.layers(baseMaps, overlayMaps, {position: 'topright'}).addTo(map);
 
   // ----------- SCALE BAR -----------
