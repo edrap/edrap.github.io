6f88f5d331f602460d5d100c9c2ee2ca62b84c57
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2022-01-24 13:50:17 +0100

    Update abruzzomap_clpv.js

--- leaflet/js/abruzzomap_clpv.js
+++ leaflet/js/abruzzomap_clpv.js
@@ -109,10 +109,20 @@ $(document).ready(function () {
     //format: "jpgpng" // server exports in either jpg or png format
   }).addTo(map).bringToBack();
   
-  //var hillshlayer = L.tileLayer('https://www.meteoaquilano.it/abruzzo/ingv_dem10m_hillshade_abr/{z}/{x}/{y}.png', {
+  // var hillshlayer = L.tileLayer('https://www.meteoaquilano.it/abruzzo/ingv_dem10m_hillshade_abr/{z}/{x}/{y}.png', {
+  //   //transparent: true,
+  //   opacity: 0.2,
+  //   maxZoom: 16,
+  //   maxNativeZoom: 16,
+  //   minNativeZoom: 10,
+  //   minZoom: 10,
+  //   tms: true,
+  //   attribution: '&copy; <a href="https://tinitaly.pi.ingv.it/" target="_blank">Tinitaly</a>'
+  // }).addTo(map);
+
   var hillshlayer = L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer/tile/{z}/{y}/{x}.png', {
     transparent: true,
-    opacity: 0.2,
+    opacity: 0.4,
     maxZoom: 16,
     maxNativeZoom: 16,
     minNativeZoom: 10,
@@ -120,7 +130,7 @@ $(document).ready(function () {
     tms: false,
     attribution: '&copy; <a href="https://tinitaly.pi.ingv.it/" target="_blank">Tinitaly</a>'
   }).addTo(map);
-
+  
   var slopelayer = L.tileLayer('https://www.meteoaquilano.it/abruzzo/ingv_dem10m_slope_abr/{z}/{x}/{y}.png', {
     //transparent: true,
     opacity: 0.4,
