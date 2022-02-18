2820ada11ad3a3f66107ab41c52cea92558c0d22
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2022-02-18 21:31:04 +0100

    Update custom_funct.js

--- leaflet/js/custom_funct.js
+++ leaflet/js/custom_funct.js
@@ -38,3 +38,21 @@ function onEachLoadedFeature(feature, layer) {
   });
   //layer.bringToFront();
 }
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
