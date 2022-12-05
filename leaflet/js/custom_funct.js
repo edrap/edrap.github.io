eaefebdea9e1a76467ca2bee27d503f8d9beaa6d
Edoardo Raparelli <edoardo.raparelli@gmail.com>
2022-12-05 15:58:42 +0100

    Update custom_funct.js

--- leaflet/js/custom_funct.js
+++ leaflet/js/custom_funct.js
@@ -1,6 +1,6 @@
 function leaflet_alert() {
     var newLine = "\r\n"
-    var brows = "The map works on Firefox and Safari. On Chrome you must allow mixed contents."
+    var brows = "" //"The map works on Firefox and Safari. On Chrome you must allow mixed contents."
     var resp = "The users take full responsibility for using the map for outdoor activities."
     var scal = "The snow cover layer may take some time to show."
     msg = brows + newLine + newLine + resp;
